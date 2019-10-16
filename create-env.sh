2ddc04bcf57b6db3696c58529552223707a1fa9e
Anees Ravid khan <anees.khan506@gmail.com>
2019-10-16 10:04:34 +0530

    replace docker to podman in create-env.sh

--- create-env.sh
+++ create-env.sh
@@ -4,8 +4,8 @@ echo "creating $cont_count containers.."
 for i in `seq $cont_count`
 do
 	echo "=============================="
-	docker run --name www.anytimeretail$i -d -it aneesravidkhan/DemoATR-img /bin/bash
+	sudo podman run --name www.anytimeretail$i -d -it localhost/aneesravidkhan/demoatr:1.01 /bin/bash
 	echo "www.anytimretail$i container has been created!"
 done
-docker inspect --format {{.NetworkSettings.IPAddress}} `docker ps -q` > IPs.txt
+#docker inspect --format {{.NetworkSettings.IPAddress}} `docker ps -q` > IPs.txt
 # ./create_lb.sh
